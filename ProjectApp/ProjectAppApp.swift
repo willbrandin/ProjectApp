13d16ed0fedb64211c48be6eb43d57938c9b1402
willbrandin <will@tidelaundry.com>
2021-02-10 11:41:55 -0700

    Initial Commit

---
+++ ProjectApp/ProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAppApp.swift
+//  ProjectApp
+//
+//  Created by Will Brandin on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
